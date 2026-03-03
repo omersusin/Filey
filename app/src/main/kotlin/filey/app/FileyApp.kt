73d3380ac851c50a508cf27ce584cbbf6ffe532d
omersusin <tekgercek73@gmail.com>
2026-03-03 10:40:20 +0300

    Fix build: remove invalid RootManager.init call

--- app/src/main/kotlin/filey/app/FileyApp.kt
+++ app/src/main/kotlin/filey/app/FileyApp.kt
@@ -1,13 +1,11 @@
 package filey.app
 
 import android.app.Application
-import filey.app.core.data.RootManager
 
 class FileyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-        RootManager.init()
     }
 
     companion object {
